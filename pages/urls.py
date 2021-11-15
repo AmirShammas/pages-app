092a9436f74e8dd36b25881543323b885a774b8a
AmirShammas <amirshammas@yahoo.com>
2021-11-16 01:41:27 +0330

    add class-based  AboutPageView

--- pages/urls.py
+++ pages/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 # from .views import homePageView
-from .views import HomePageView
+from .views import HomePageView, AboutPageView
 
 """ urlpatterns = [
     path('', homePageView, name='home'),
@@ -9,4 +9,5 @@ from .views import HomePageView
 
 urlpatterns = [
     path('', HomePageView.as_view(), name='home'),
+    path('about/', AboutPageView.as_view(), name='about'),
 ]
